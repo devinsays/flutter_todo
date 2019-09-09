35a33795b6e1f3ee2ba4b05b71e9ad76c2e41553
Devin Price <devin.price@nanoglobal.com>
2019-09-09 14:34:55 -0500

    Auth provider v1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_todo/views/loading.dart';
+import 'package:flutter_todo/models/auth.dart';
 import 'package:flutter_todo/views/login.dart';
 import 'package:flutter_todo/views/register.dart';
 import 'package:flutter_todo/views/password_reset.dart';
@@ -10,7 +12,7 @@ void main() {
     MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => Loading(),
+        '/': (context) => Router(),
         '/login': (context) => LogIn(),
         '/register': (context) => Register(),
         '/password-reset': (context) => PasswordReset(),
@@ -18,4 +20,29 @@ void main() {
       },
     ),
   );
+}
+
+class Router extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (_) => AuthRepository(),
+      child: Consumer(
+        builder: (context, AuthRepository user, _) {
+          switch (user.status) {
+            case Status.Uninitialized:
+              return Loading();
+            case Status.Unauthenticated:
+              return LogIn();
+            case Status.Authenticating:
+              return LogIn();
+            case Status.Authenticated:
+              return Todos();
+            default:
+              return LogIn();
+          }
+        },
+      ),
+    );
+  }
 }
